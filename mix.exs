4d33beb98ee947b8572f86ab043883c5cb626216
Michael Pope <mpope.cr@gmail.com>
2017-05-24 23:07:45 -0700

    Add ex_doc

--- mix.exs
+++ mix.exs
@@ -1,34 +1,31 @@
 defmodule Capture.Mixfile do
   use Mix.Project
 
-  def project do
-    [app: :capture,
-     version: "0.1.0",
-     elixir: "~> 1.4",
-     build_embedded: Mix.env == :prod,
-     start_permanent: Mix.env == :prod,
-     deps: deps()]
+  def application() do
+    [
+      extra_applications: [:logger],
+      mod: {Capture.Application, []},
+    ]
   end
 
-  # Configuration for the OTP application
-  #
-  # Type "mix help compile.app" for more information
-  def application do
-    # Specify extra applications you'll use from Erlang/Elixir
-    [extra_applications: [:logger],
-     mod: {Capture.Application, []}]
+  def project() do
+    [
+      app: :capture,
+      build_embedded: Mix.env == :prod,
+      deps: deps(),
+      elixir: "~> 1.4",
+      start_permanent: Mix.env == :prod,
+      version: "0.1.0",
+    ]
   end
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:my_dep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:my_dep, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
-  #
-  # Type "mix help deps" for more examples and options
-  defp deps do
-    []
+  ###########
+  # Private #
+  ###########
+
+  defp deps() do
+    [
+      {:ex_doc, "~> 0.14", only: :dev, runtime: false},
+    ]
   end
 end
