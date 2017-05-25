da88d4115f492e1616943482fcd3284e199eac97
Michael Pope <mpope.cr@gmail.com>
2017-05-24 23:39:53 -0700

    Delete hello world boilerplate

--- lib/capture.ex
+++ lib/capture.ex
@@ -2,17 +2,4 @@ defmodule Capture do
   @moduledoc """
   Documentation for Capture.
   """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Capture.hello
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end
--- test/capture_test.exs
+++ test/capture_test.exs
@@ -1,8 +1,3 @@
 defmodule CaptureTest do
   use ExUnit.Case
-  doctest Capture
-
-  test "the truth" do
-    assert 1 + 1 == 2
-  end
 end
